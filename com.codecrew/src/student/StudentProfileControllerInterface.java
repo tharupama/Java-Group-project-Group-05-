1437036da79a1c63b2ff7e1b983d234132f7f6d2
binoy969 <binoymahima1234@gmail.com>
2026-04-25 09:04:00 +0530

    implement StudentProfileEncapsulated model class
    - Created encapsulated class for student profile data
    - Added fields: contact, email, profile image, and student ID
    - Implemented constructors (default and parameterized)
    - Added getters and setters for all fields
    - Supports clean data transfer between UI and controller layers

---
+++ com.codecrew/src/student/StudentProfileControllerInterface.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.codecrew.view;
+
+import java.sql.SQLException;
+
+/**
+ *
+ * @author USER
+ */
+public interface StudentProfileControllerInterface {
+    void updateStudent(StudentProfileEncapsulated studentProfileEncapsulated) throws ClassNotFoundException, SQLException ;
+}
